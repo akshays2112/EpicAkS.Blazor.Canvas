c67ce2d0a654ee6c3aaf8395b6c3a7c3f4360cd4
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2023-02-16 05:35:50 +0530

    Temporarily saving this work to rethink Scrollbar vis a vie controls in this case ListBox

---
+++ EpicAkS.Blazor.Canvas.Components/BasicControls/Classes/VerticalScrollBar.cs
@@ -0,0 +1,25 @@
+﻿namespace EpicAkS.Blazor.Canvas.Components.BasicControls.Classes;
+
+public class VerticalScrollBar
+{
+    public int Width { get; set; } = 11;
+
+    public int Height { get; set; } = 100;
+
+    public int MaxItems { get; set; } = 0;
+
+    public int CurrentItem { get; set; } = 0;
+
+    public string ThemeColor { get; set; } = "gray";
+
+    public async Task DrawVerticalScrollBar(Helpers.CanvasComponentInfo canvasComponentInfo, int X, int Y)
+    {
+        if (canvasComponentInfo is null) return;
+        CanvasClass? canvas = canvasComponentInfo.Canvas;
+        if (canvas is null) return;
+        await canvas.SetFillStyle(ThemeColor);
+        await canvas.FillRect(X, Y, Width, Height);
+        await canvas.SetFillStyle("darkgray");
+        await canvas.FillRect(X, Y + ((CurrentItem * Height) / MaxItems), Width, 10);
+    }
+}
